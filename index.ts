a2abec995df45dc03f6f8b77d9b0dad3f534b4d2
Lucas Calvetti <lic.calvo099@gmail.com>
2021-09-25 00:25:09 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("dist"));
+
+// handler
+app.get("/env", (req, res) => {
+    res.json({
+        environment: process.env.NODE_ENV,
+    });
+});
+
+app.get("/db-env", (req, res) => {
+    res.json({
+        "db-host": process.env.DB_HOST,
+    });
+});
+
+app.get("/hola", (req, res) => {
+    res.json({
+        message: "hola soy marce",
+    });
+});
+
+app.listen(port, () => {
+    console.log("hola soy express y estoy corriendo en el puerto " + port);
+});
